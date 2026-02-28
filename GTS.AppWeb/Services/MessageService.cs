5cf6924442e0167ab09a9967de4187eba3acc6c6
Julian Mostbauer <julian.mostbauer@outlook.com>
2026-02-28 14:23:00 +0100

    routing problems fixed

--- GTS.AppWeb/Services/MessageService.cs
+++ GTS.AppWeb/Services/MessageService.cs
@@ -1,22 +1,20 @@
 using GTS.Core;
+namespace GTS.AppWeb.Services;
 
-namespace GTS.AppWeb.Services
+public class MessageService
 {
-    public class MessageService
+    public Message[]? Messages { get; private set; }
+    public string[]? Senders { get; private set; }
+
+    public void SetMessages(Message[]? messages)
     {
-        public Message[]? Messages { get; private set; }
-        public string[]? Senders { get; private set; }
+        Messages = messages;
+        Senders = messages?.Select(m => m.Sender).Distinct().ToArray();
+    }
 
-        public void SetMessages(Message[]? messages)
-        {
-            Messages = messages;
-            Senders = messages?.Select(m => m.Sender).Distinct().ToArray();
-        }
-
-        public void ClearMessages()
-        {
-            Messages = null;
-            Senders = null;
-        }
+    public void ClearMessages()
+    {
+        Messages = null;
+        Senders = null;
     }
 }
